a252b2acc4b7183f900fd3330d4b782d342c066e
ACarling <aidanc@internode.on.net>
2025-03-28 13:55:40 +1100

    can survive internet going down

--- ddns.py
+++ ddns.py
@@ -3,19 +3,26 @@ import route53
 import requests
 import time
 
+import dotenv
+
+dotenv.load_dotenv()
+
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_ACCESS_SECRET = os.getenv("AWS_ACCESS_SECRET")
 A_RECORD_NAME = os.getenv("A_RECORD_NAME")
 POLL_TIMEOUT_SECONDS = os.getenv("POLL_TIMEOUT_SECONDS")
+HOSTED_ZONE_ID=os.getenv("HOSTED_ZONE_ID")
 
-conn = route53.connect(
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_ACCESS_SECRET,
-)
 
-cachedIp = ""
+def main():
+    conn = route53.connect(
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_ACCESS_SECRET,
+    )
 
-for zone in conn.list_hosted_zones():
+    cachedIp = ""
+
+    zone = conn.get_hosted_zone_by_id(HOSTED_ZONE_ID)
     for record_set in zone.record_sets:
         if A_RECORD_NAME in record_set.name:
             print("Retrieved Ip: ", record_set.name, " ", record_set.records)
@@ -26,18 +33,38 @@ for zone in conn.list_hosted_zones():
                 record_set.records[0] = cachedIp
                 record_set.save()
 
-print("======= STARTING UPDATE LOOP =======")
-while True:
-    IP = requests.get("https://api.ipify.org").text
-    print("Reported IP: ", IP)
-    if IP != cachedIp:
-        for zone in conn.list_hosted_zones():
+    print("======= STARTING UPDATE LOOP =======")
+    while True:
+        req = requests.get("https://api.ipify.org")
+        if req.status_code != 200:
+            Exception("ERR: Ip readback service reported status: ", req.status_code)
+        
+        IP = req.text
+        if IP != cachedIp:
+            print(time.asctime())
+            zone = conn.get_hosted_zone_by_id(HOSTED_ZONE_ID)
             for record_set in zone.record_sets:
                 if A_RECORD_NAME in record_set.name:
-                    print("# ", record_set.name, " ", record_set.records)
+                    print(" > Cached IP and public ip mismatch, dns lookup says: ", record_set.name, " ", record_set.records)
                     if record_set.records[0] != IP:
-                        print(" |- Updating Ip for: ", record_set.name, " ", record_set.records, " -> ", cachedIp)
+                        print("     |- Updating Ip for: ", record_set.name, " ", record_set.records, " -> ", IP)
                         record_set.records[0] = IP
                         record_set.save()
-        cachedIp = IP
+                    else:
+                        print("     |- no need to update: ")
+
+            cachedIp = IP
+            print()
+        time.sleep(int(POLL_TIMEOUT_SECONDS))
+
+# Hangs on network reset
+while True:
+    try:
+        main()
+    except Exception as e:
+        print("ERR (", time.asctime(), ") : ", repr(e))
+        print()
+        if "403" in str(e):
+            print("CRITICAL ERR (", time.asctime(), ") : update iam policy, exiting ...")
+            break 
     time.sleep(int(POLL_TIMEOUT_SECONDS))
